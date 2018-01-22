b948f8f3657160637d4b437dcf35a9451e35b3a2
Jacob Mikkelsen <jacob.n.mikkelsen@gmail.com>
2018-01-22 12:26:55 +1100

    - Moved into main function

--- roomfinder.py
+++ roomfinder.py
@@ -28,40 +28,43 @@ def timeToValue(dt):
     
     return 2 * hour + half
 
-#TODO URL includes database set - would want to update over time?
-url = "http://nss.cse.unsw.edu.au/tt/find_rooms.php?dbafile=2018-KENS-COFA.DBA&campus=KENS"
+def main ():
+    #TODO URL includes database set - would want to update over time?
+    url = "http://nss.cse.unsw.edu.au/tt/find_rooms.php?dbafile=2018-KENS-COFA.DBA&campus=KENS"
 
-dt = datetime.datetime.now()
+    dt = datetime.datetime.now()
 
-currentTime = timeToValue(dt)
+    currentTime = timeToValue(dt)
 
-fr_time = str(currentTime)
-to_time = str(currentTime + 2)
+    fr_time = str(currentTime)
+    to_time = str(currentTime + 2)
 
-days = dt.strftime("%A")
-# TODO %e not supported by Windows?
-fr_date = dt.strftime("%a %e %b %Y")
-to_date = fr_date
+    days = dt.strftime("%A")
+    # TODO %e not supported by Windows?
+    fr_date = dt.strftime("%a %e %b %Y")
+    to_date = fr_date
 
-print("Date: " + fr_date)
+    print("Date: " + fr_date)
 
-print("Times between " + fr_time + " and " + to_time)
+    print("Times between " + fr_time + " and " + to_time)
 
-# Maybe the Requests library or whatever makes constructing this nicer?
-post_fields = {
-                "search_rooms": "Search",
-                "days[]": days,
-                "fr_date": fr_date,
-                "fr_time": fr_time,
-                "to_date": to_date,
-                "to_time": to_time,
-                "RU[]": ["RU_GP-LEC", "RU_GP-TUTSEM"]
-            }
+    # Maybe the Requests library or whatever makes constructing this nicer?
+    post_fields = {
+                    "search_rooms": "Search",
+                    "days[]": days,
+                    "fr_date": fr_date,
+                    "fr_time": fr_time,
+                    "to_date": to_date,
+                    "to_time": to_time,
+                    "RU[]": ["RU_GP-LEC", "RU_GP-TUTSEM"]
+                }
 
-contents = getPage(url, post_fields)
-rooms = parsePage(contents)
+    contents = getPage(url, post_fields)
+    rooms = parsePage(contents)
 
-print ("Found " + str(len(rooms)) + " rooms")
+    print ("Found " + str(len(rooms)) + " rooms")
 
-for room in rooms:
-    print(room[1])
+    for room in rooms:
+        print(room[1])
+
+main()
