004bdb6200253fbd9c23651e2d342bacc59beb0c
Jacob Mikkelsen <jacob.n.mikkelsen@gmail.com>
2018-01-23 18:14:14 +1100

    - Now getting current time and date from shell for timezone reasons

--- roomfinder.py
+++ roomfinder.py
@@ -3,6 +3,9 @@
 import urllib
 import httplib
 
+import os
+import subprocess
+
 import re
 import datetime
 
@@ -38,27 +41,73 @@ def parsePage (contents):
     
     return rooms
 
-def timeToValue(dt):
-    hour = dt.hour
-    half = 1 if dt.minute >= 30 else 0
+def timeToValue(hour, minute):
+    half = 1 if minute >= 30 else 0
     
     return 2 * hour + half
+
+def getTimeDate ():
+    env = os.environ.copy()
+    env["TZ"] = "Australia/Sydney"
+    
+    timeFormat = ["%a", "%d", "%b", "%Y", "%A", "%H", "%M"]
+    
+    p = subprocess.Popen(["date", "+" + ' '.join(timeFormat)], env=env, stdout=subprocess.PIPE)
+    output, _ = p.communicate()
+    
+    # print("Got output:")
+    # print(output.rstrip())
+    
+    sections = re.findall("(\S+)", output.rstrip())
+    print(sections)
+    
+    # Check if we have same number of matches as time format
+    if len(sections) != len(timeFormat):
+        print("Unexpected number of date outputs")
+        print("Format: " + str(timeFormat))
+        print("Output: " + str(sections))
+        exit(1)
+    
+    timeInfo = {}
+    
+    # TODO Chuck this all into a datetime object instead? Probably a nicer way
+    # of working with it....
+    
+    for i in range(len(timeFormat)):
+        timeInfo[timeFormat[i]] = sections[i]
+    
+    print(timeInfo)
+    
+    return timeInfo
+    
+    
 
 def main ():
     #TODO URL includes database set - would want to update over time?
     url = "http://nss.cse.unsw.edu.au/tt/find_rooms.php?dbafile=2018-KENS-COFA.DBA&campus=KENS"
 
     # TODO GOSH DARN TIMEZONES
+    # Maybe we could handle this via javascript? Set the dropdown options to be
+    # the local user's time?
     dt = datetime.datetime.now()
+    
+    # To get current hour in Sydney (24h mode), in shell:
+    # TZ=Australia/Sydney date +"%H"
+    # Run as another process, capture output, bingo?
+    # Add other format options to grab more information?
+    
+    timeInfo = getTimeDate()
+    
+    # Need to close the subprocess or anything?
 
-    currentTime = timeToValue(dt)
+    currentTime = timeToValue(int(timeInfo["%H"]), int(timeInfo["%M"]))
 
     fr_time = str(currentTime)
     to_time = str(currentTime + 2)
 
-    days = dt.strftime("%A")
+    days = timeInfo["%A"]
     # TODO %e not supported by Windows?
-    fr_date = dt.strftime("%a %e %b %Y")
+    fr_date = ' '.join([timeInfo["%a"], timeInfo["%d"], timeInfo["%b"], timeInfo["%Y"]])
     to_date = fr_date
 
     print("Date: " + fr_date)
